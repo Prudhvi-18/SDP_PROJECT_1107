4611c6b9dff0966d959ae27f2154dddda77f18f8
Prudhvi-18 <prudhvigarapati@gmail.com>
2023-11-05 08:17:24 +0530

    commit

---
+++ myapp/models.py
@@ -0,0 +1,75 @@
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+from .constants import PaymentStatus
+
+# Create your models here.
+
+class Bus(models.Model):
+    bus_name = models.CharField(max_length=30)
+    source = models.CharField(max_length=30)
+    dest = models.CharField(max_length=30)
+    nos = models.DecimalField(decimal_places=0, max_digits=2)
+    rem = models.DecimalField(decimal_places=0, max_digits=2)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    date = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.bus_name
+
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField()
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.email
+
+
+class Book(models.Model):
+    BOOKED = 'B'
+    CANCELLED = 'C'
+
+    TICKET_STATUSES = ((BOOKED, 'Booked'),
+                       (CANCELLED, 'Cancelled'),)
+    email = models.EmailField()
+    name = models.CharField(max_length=30)
+    userid =models.DecimalField(decimal_places=0, max_digits=2)
+    busid=models.DecimalField(decimal_places=0, max_digits=2)
+    bus_name = models.CharField(max_length=30)
+    source = models.CharField(max_length=30)
+    dest = models.CharField(max_length=30)
+    nos = models.DecimalField(decimal_places=0, max_digits=2)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.CharField(choices=TICKET_STATUSES, default=BOOKED, max_length=2)
+
+    def __str__(self):
+        return self.email
+
+
+class Order(models.Model):
+    name = models.CharField(_("Customer Name"), max_length=254, blank=False, null=False)
+    amount = models.FloatField(_("Amount"), null=False, blank=False)
+    status = models.CharField(
+        _("Payment Status"),
+        default=PaymentStatus.PENDING,
+        max_length=254,
+        blank=False,
+        null=False,
+    )
+    provider_order_id = models.CharField(
+        _("Order ID"), max_length=40, null=False, blank=False
+    )
+    payment_id = models.CharField(
+        _("Payment ID"), max_length=36, null=False, blank=False
+    )
+    signature_id = models.CharField(
+        _("Signature ID"), max_length=128, null=False, blank=False
+    )
+
+    def str(self):
+        return f"{self.id}-{self.name}-{self.status}"
---
+++ myapp/views.py
@@ -0,0 +1,224 @@
+from django.shortcuts import render
+from decimal import Decimal
+
+import razorpay
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+import json
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+
+from .constants import PaymentStatus
+from .models import User, Bus, Book, Order
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .forms import UserLoginForm, UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from decimal import Decimal
+
+
+
+
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, 'myapp/home.html')
+    else:
+        return render(request, 'myapp/signin.html')
+
+
+@login_required(login_url='signin')
+def findbus(request):
+    context = {}
+    if request.method == 'POST':
+        source_r = request.POST.get('source')
+        dest_r = request.POST.get('destination')
+        date_r = request.POST.get('date')
+        bus_list = Bus.objects.filter(source=source_r, dest=dest_r, date=date_r)
+        if bus_list:
+            return render(request, 'myapp/list.html', locals())
+        else:
+            context["error"] = "Sorry no buses availiable"
+            return render(request, 'myapp/findbus.html', context)
+    else:
+        return render(request, 'myapp/findbus.html')
+
+
+@login_required(login_url='signin')
+def bookings(request):
+    context = {}
+    if request.method == 'POST':
+        id_r = request.POST.get('bus_id')
+        seats_r = int(request.POST.get('no_seats'))
+        bus = Bus.objects.get(id=id_r)
+        if bus:
+            if bus.rem > int(seats_r):
+                name_r = bus.bus_name
+                cost = int(seats_r) * bus.price
+                source_r = bus.source
+                dest_r = bus.dest
+                nos_r = Decimal(bus.nos)
+                price_r = bus.price
+                date_r = bus.date
+                time_r = bus.time
+                username_r = request.user.username
+                email_r = request.user.email
+                userid_r = request.user.id
+                rem_r = bus.rem - seats_r
+                Bus.objects.filter(id=id_r).update(rem=rem_r)
+                book = Book.objects.create(name=username_r, email=email_r, userid=userid_r, bus_name=name_r,
+                                           source=source_r, busid=id_r,
+                                           dest=dest_r, price=price_r, nos=seats_r, date=date_r, time=time_r,
+                                           status='BOOKED')
+                print('------------book id-----------', book.id)
+                # book.save()
+                return render(request, 'myapp/bookings.html', locals())
+            else:
+                context["error"] = "Sorry select fewer number of seats"
+                return render(request, 'myapp/findbus.html', context)
+
+    else:
+        return render(request, 'myapp/findbus.html')
+
+
+@login_required(login_url='signin')
+def cancellings(request):
+    context = {}
+    if request.method == 'POST':
+        id_r = request.POST.get('bus_id')
+        # seats_r = int(request.POST.get('no_seats'))
+
+        try:
+            book = Book.objects.get(id=id_r)
+            bus = Bus.objects.get(id=book.busid)
+            rem_r = bus.rem + book.nos
+            Bus.objects.filter(id=book.busid).update(rem=rem_r)
+            # nos_r = book.nos - seats_r
+            Book.objects.filter(id=id_r).update(status='CANCELLED')
+            Book.objects.filter(id=id_r).update(nos=0)
+            return redirect(seebookings)
+        except Book.DoesNotExist:
+            context["error"] = "Sorry You have not booked that bus"
+            return render(request, 'myapp/error.html', context)
+    else:
+        return render(request, 'myapp/findbus.html')
+
+
+@login_required(login_url='signin')
+def seebookings(request, new={}):
+    context = {}
+    id_r = request.user.id
+    book_list = Book.objects.filter(userid=id_r)
+    if book_list:
+        return render(request, 'myapp/booklist.html', locals())
+    else:
+        context["error"] = "Sorry no buses booked"
+        return render(request, 'myapp/findbus.html', context)
+
+
+def signup(request):
+    context = {}
+    if request.method == 'POST':
+        name_r = request.POST.get('name')
+        email_r = request.POST.get('email')
+        password_r = request.POST.get('password')
+        user = User.objects.create_user(name_r, email_r, password_r, )
+        if user:
+            login(request, user)
+            return render(request, 'myapp/thank.html')
+        else:
+            context["error"] = "Provide valid credentials"
+            return render(request, 'myapp/signup.html', context)
+    else:
+        return render(request, 'myapp/signup.html', context)
+
+
+def signin(request):
+    context = {}
+    if request.method == 'POST':
+        name_r = request.POST.get('name')
+        password_r = request.POST.get('password')
+        user = authenticate(request, username=name_r, password=password_r)
+        if user:
+            login(request, user)
+            # username = request.session['username']
+            context["user"] = name_r
+            context["id"] = request.user.id
+            return render(request, 'myapp/success.html', context)
+            # return HttpResponseRedirect('success')
+        else:
+            context["error"] = "Provide valid credentials"
+            return render(request, 'myapp/signin.html', context)
+    else:
+        context["error"] = "You are not logged in"
+        return render(request, 'myapp/signin.html', context)
+
+
+def signout(request):
+    context = {}
+    logout(request)
+    context['error'] = "You have been logged out"
+    return render(request, 'myapp/signin.html', context)
+
+
+def success(request):
+    context = {}
+    context['user'] = request.user
+    return render(request, 'myapp/success.html', context)
+
+
+def make_payment(request):
+    return render(request, "myapp/pay.html")
+
+
+def order_payment(request):
+    if request.method == "POST":
+        name = request.POST.get("bus_id")
+        amount = request.POST.get("seat_no")
+        amount = int(amount) * 250
+        client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET))
+        razorpay_order = client.order.create({"amount": int(amount) * 100, "currency": "INR", "payment_capture": "1"})
+        order = Order.objects.create(name=name, amount=amount, provider_order_id=razorpay_order["id"])
+        order.save()
+        return render(
+            request,
+            "myapp/payment.html",
+            {
+                "callback_url": "http://" + "localhost:8000" + "/callback/",
+                "razorpay_key": settings.RAZORPAY_KEY_ID,
+                "order": order,
+            },
+        )
+    return render(request, "myapp/payment.html")
+
+
+@csrf_exempt
+def callback(request):
+    def verify_signature(response_data):
+        client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET))
+        return client.utility.verify_payment_signature(response_data)
+
+    if "razorpay_signature" in request.POST:
+        payment_id = request.POST.get("razorpay_payment_id", "")
+        provider_order_id = request.POST.get("razorpay_order_id", "")
+        signature_id = request.POST.get("razorpay_signature", "")
+        order = Order.objects.get(provider_order_id=provider_order_id)
+        order.payment_id = payment_id
+        order.signature_id = signature_id
+        order.save()
+        if verify_signature(request.POST):
+            order.status = PaymentStatus.SUCCESS
+            order.save()
+            return render(request, "myapp/callback.html", context={"status": order.status})
+        else:
+            order.status = PaymentStatus.FAILURE
+            order.save()
+            return render(request, "myapp/callback.html", context={"status": order.status})
+    else:
+        payment_id = json.loads(request.POST.get("error[metadata]")).get("payment_id")
+        provider_order_id = json.loads(request.POST.get("error[metadata]")).get("order_id")
+        order = Order.objects.get(provider_order_id=provider_order_id)
+        order.payment_id = payment_id
+        order.status = PaymentStatus.FAILURE
+        order.save()
+        return render(request, "myapp/callback.html", context={"status": order.status})
+
